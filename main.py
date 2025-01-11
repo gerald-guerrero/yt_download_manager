6b787cda99c043dd940cbb9cc07a70f87875b3b8
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-11 14:56:59 -0500

    Implemented validation checks and defaults for user input

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import argparse
+import os
+import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument("url", help="URL of the Youtube video you want to download")
-parser.add_argument("--output", help="Destination directory for the video")
-parser.add_argument("--resolution", help="Chosen resolution for video")
+
+def url_check(url):
+    # Checks if URL has key words to determine if it is a Youtube URL
+    if "youtube.com" not in url:
+         parser.error("Not A Youtube URL. Please try again")       
+    return url
+
+def output_check(output):
+    # Checks if directory path exists and creats directory if it does not
+    if not os.path.exists(output):
+        try:
+            print("Directory does not exist\nCreating Directory")
+            os.mkdir(output)
+        except:
+            parser.error("Not a valid directory. Please try again")
+    
+    return output
+
+def resolution_check(resolution):
+     # Will check if resolution is a valid resolution
+     res_pattern = r"[0-9]?[0-9]{3}p"
+     if not re.fullmatch(res_pattern, resolution):
+         resolution = "720p"
+         print("Not a valid resolution. Defaulting to 720p")
+     return resolution
+
+
+parser.add_argument("url", help="URL of the Youtube video you want to download", type=url_check)
+parser.add_argument("--output", help="Destination directory for the video", type=output_check)
+parser.add_argument("--resolution", help="Chosen resolution for video", type=resolution_check)
+parser.set_defaults(output=os.path.join(os.getcwd(), "downloads"), resolution = "720p")
 args = parser.parse_args()
 
 url = args.url
