50c26238c75dc284c6c6e0b66a9f8073f8c670f4
Gerald Guerrero <geraldguerrerotech@gmail.com>
2025-01-13 19:45:18 -0500

    improved documentation

--- README.md
+++ README.md
@@ -11,5 +11,7 @@ pytube
 5. Activate the virtual environment with `venv\Scripts\activate`
 6. Upgrade pip with `python -m pip install --upgrade pip`
 7. Install dependencies with `pip install -r requirements.txt`
+8. Run app with command `python main.py "youtube_url_here"`
+9. Optional arguments `--output "directory_path_here"` and `--resolution "res_here"` can also be included
 
 You can deactivate the virtual environment by entering the command `deactivate`
--- main.py
+++ main.py
@@ -7,13 +7,19 @@ from pytubefix.exceptions import VideoUnavailable
 parser = argparse.ArgumentParser()
 
 def url_check(url):
-    # Checks if URL has key words to determine if it is a Youtube URL
+    """
+    Checks if URL has key words to determine if it is a Youtube URL
+    Returns the URL if keywords are found and a parser error if they are not found
+    """
     if "youtube.com" not in url and "youtu.be" not in url:
-         parser.error("Not A Youtube URL. Please try again")       
+        parser.error("Not A Youtube URL. Please try again")       
     return url
 
 def output_check(output):
-    # Checks if directory path exists and creats directory if it does not
+    """
+    Checks if directory path exists and creates directory if it does not
+    returns a parser error if the directory cannot be created
+    """
     if not os.path.exists(output):
         try:
             print("Directory does not exist\nCreating Directory")
@@ -24,58 +30,72 @@ def output_check(output):
     return output
 
 def resolution_check(resolution):
-     # Will check if resolution is a valid resolution
+     """
+     Will check with regex comparison if resolution is using a valid format
+     If it is not a valid resolution, it will use parser default
+     """
      res_pattern = r"[0-9]?[0-9]{3}p"
      if not re.fullmatch(res_pattern, resolution):
-         resolution = "720p"
-         print("Not a valid resolution. Please try again")
+         print("Not a valid resolution. will use default resolution")
+         resolution = parser.get_default("resolution")
+         
      return resolution
 
 def video_progress(stream, data_chunk, bytes_remaining):
+    """
+    Calculates the percent completion of the download and prints the progress for the user
+    """
     current_bytes = stream.filesize - bytes_remaining
     progress = int((current_bytes / stream.filesize) * 100)
     print(f"{progress}%")
 
 def video_completed(stream, file_path):
-    print("Download completed.\nLocation:", file_path)
+    """
+    prints a download completed message and the location of the downloaded file
+    """
+    print(f"Download completed.\nLocation: {file_path}\n")
 
+# Adds required argument, url, and calls url_check function to ensure the url is valid
 parser.add_argument(
     "url", 
     help="URL of the Youtube video. Use format: \"URL\"", 
     type=url_check
     )
+# Adds optional argument, --output, and calls output_check funtion to ensure directory exists
 parser.add_argument(
     "--output", 
     help="Destination directory for downloads. Use format: --url \"path\"", 
     type=output_check
     )
+# Adds optional argument, --resolution, and calls resolution_check to ensure resolution is valid
 parser.add_argument(
     "--resolution", 
     help="Chosen resolution for video. Use format: --resolution ###p or ####p",
     type=resolution_check
     )
-parser.set_defaults(output=os.path.join(os.getcwd(), "downloads"), resolution = "720p")
+# Sets default values for output and resolution if either is left blank
+parser.set_defaults(output=os.path.join(os.getcwd(), "downloads"), resolution = "480p")
 args = parser.parse_args()
 
 url = args.url
 output = args.output
 resolution = args.resolution
 
-try:
+try: # Searches for video based on URL and filters for resolution and files with audio and video
     yt = YouTube(url, on_progress_callback=video_progress, on_complete_callback=video_completed)
     video = yt.streams.filter(resolution=resolution, progressive=True).first()
-except VideoUnavailable as e:
+except VideoUnavailable as e: # Raises error if URL is invalid
     print("\nInvalid URL. Please try again with a valid URL\n")
     raise e
 
-if video is None:
-    print("Chosen resolution not available\nDefaulting to highest available")
+if video is None: # If no videos are found, it will default to getting the highest resolution
+    print("\nChosen resolution not available\nDefaulting to highest resolution available")
     video = yt.streams.filter(progressive=True).get_highest_resolution()
     resolution = video.resolution
 
 print(f"\nDownload Information:\n url: {url}\n output: {output}\n resolution: {resolution}\n")
 
-try:
+try: # Begins downloading the video
     video.download(output_path=output)
-except:
+except: # Raises an error if the download has failed or is interrupted
     raise Exception("Download has failed. Please try again")
